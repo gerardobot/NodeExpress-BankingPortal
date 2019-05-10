aead3befb87b55bc8157e72ccbdada96d4ddc507
Gerardo <tugerardovirtual@gmail.com>
2019-05-10 09:17:32 +0200

    module3 solution

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 const accountData = fs.readFileSync(
 	path.join(__dirname, 'json', 'accounts.json'),
@@ -41,6 +42,52 @@ app.get('/credit', (request, response) => {
 
 app.get('/profile', (request, response) => {
 	response.render('profile', { user: users[0] });
+});
+
+app.get('/payment', (request, response) => {
+	response.render('payment', { account: accounts.credit });
+});
+
+app.post('/payment', (request, response) => {
+	accounts.credit.balance -= request.body.amount;
+	accounts.credit.available =
+		parseInt(accounts.credit.available) + parseInt(request.body.amount);
+	response.render('payment', { account: accounts.credit });
+	const accountsJSON = JSON.stringify(accounts);
+	fs.copyFileSync(
+		path.join(__dirname, 'json', 'accounts.json'),
+		path.join(__dirname, 'json', 'accounts_backup.json')
+	);
+	fs.writeFileSync(
+		path.join(__dirname, 'json', 'accounts.json'),
+		accountsJSON,
+		'utf8'
+	);
+	response.render('payment', {
+		message: 'Payment Successful',
+		account: accounts.credit
+	});
+});
+
+app.get('/transfer', (request, response) => {
+	response.render('transfer');
+});
+
+app.post('/transfer', (request, response) => {
+	accounts[request.body.from].balance -= request.body.amount;
+	accounts[request.body.to].balance =
+		parseInt(accounts[request.body.to].balance) + parseInt(request.body.amount);
+	const accountsJSON = JSON.stringify(accounts);
+	fs.copyFileSync(
+		path.join(__dirname, 'json', 'accounts.json'),
+		path.join(__dirname, 'json', 'accounts_backup.json')
+	);
+	fs.writeFileSync(
+		path.join(__dirname, 'json', 'accounts.json'),
+		accountsJSON,
+		'utf8'
+	);
+	response.render('transfer', { message: 'Transfer Completed' });
 });
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
